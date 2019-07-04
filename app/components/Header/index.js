4f827542a70803940c030eecb905771dbe2a0a26
Amnish Sthapit <amnish138@gmail.com>
2019-07-04 16:35:47 +0545

    homepage design

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './assets/header.scss';
 import logo from './assets/logo.png';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { makeSelectOrgInfo } from '../../containers/App/selectors';
 import Search from 'containers/Search/Loadable';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
-import fb from './assets/facebook.svg';
-import tw from './assets/twitter.svg';
-import yt from './assets/youtube.svg';
-import ins from './assets/instagram.svg';
 
 class Header extends React.PureComponent {
   render() {
@@ -20,61 +16,29 @@ class Header extends React.PureComponent {
 
     return (
       <header id="header">
-        <div className="top__header ">
-          <div className="container">
-            <div className="d-flex align-items-center">
-              <h5>Broadlink</h5>
-              <div className="social__menu ml-auto">
-                {orgInfoData && orgInfoData.facebook && (
-                  <a target="_blank" href={orgInfoData?.facebook}>
-                    <img src={fb} alt="" />
-                  </a>
-                )}
-                {orgInfoData && orgInfoData.twitter && (
-                  <a target="_blank" href={orgInfoData?.twitter}>
-                    <img src={tw} alt="" />
-                  </a>
-                )}
-                {orgInfoData && orgInfoData.youtube && (
-                  <a target="_blank" href={orgInfoData?.youtube}>
-                    <img src={yt} alt="" />
-                  </a>
-                )}
-                {orgInfoData && orgInfoData.instagram && (
-                  <a target="_blank" href={orgInfoData?.instagram}>
-                    <img src={ins} alt="" />
-                  </a>
-                )}
-                {/* <a
-                  target="_blank"
-                  href="https://www.instagram.com/thegivingbrick/"
-                >
-                  <img src={ins} alt="" />
-                </a> */}
-              </div>
-            </div>
-          </div>
-        </div>
         <Navbar expand="lg">
           <Container>
             <Link className="navbar-brand" to="/">
-              <img className="img-fluid" src={logo} alt="" />
+              {/* <img className="img-fluid" src={logo} alt="" /> */}
+              <h1 style={{ color: '#fff' }}>PCSC</h1>
             </Link>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
+              <Nav>
                 <NavLink exact className="nav-link" to="/">
-                  Home
+                  Services
                 </NavLink>
                 <NavLink exact className="nav-link" to="/about-us">
                   ABOUT US
                 </NavLink>
-                <NavLink exact className="nav-link" to="/help-us">
-                  HELP US
+              </Nav>
+            </Navbar.Collapse>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <NavLink exact className="nav-link" to="/">
+                  Register
                 </NavLink>
-                <NavLink exact className="nav-link" to="/contact-us">
-                  CONTACT US
-                </NavLink>
+                <Button className="btn__primary">Login</Button>
               </Nav>
             </Navbar.Collapse>
           </Container>
