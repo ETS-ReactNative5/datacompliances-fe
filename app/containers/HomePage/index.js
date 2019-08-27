a56c4cde5be3c387c55bd29d7c2dd94971dee328
momika <shr.momika@gmail.com>
2019-08-27 14:47:02 +0545

    responsive fixes for product slider

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -41,10 +41,37 @@ var settings = {
   slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: false,
-      speed: 500,
-      autoplaySpeed: 2000,
-      cssEase: "ease-in",
-      centerPadding: 30
+  speed: 500,
+  autoplaySpeed: 2000,
+  cssEase: "ease-in",
+  responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]  
+
+
 };
 
 /* eslint-disable react/no-multi-comp */
