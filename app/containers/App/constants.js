49e380a7724d9ce44315ca93841704b18e5435e1
Shrawan Lakhe <shrawanlakhey@gmail.com>
2019-09-02 09:30:20 +0545

    merge conflict issue fixes

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -1,8 +1,8 @@
 const isProdEnv = process.env.NODE_ENV === 'production';
 
 export const API_BASE = isProdEnv
-  ? 'https://www.datacompliances.com/api/'
-  : 'https://www.datacompliances.com/api/';
+  ? 'http://dev.datacompliances.com/api/'
+  : 'http://localhost:4001/api/';
 
 export const GOOGLE_CLIENT_ID =
   '632673995527-997dv6bet048loapcqgtfpfbqubslr2l.apps.googleusercontent.com';
