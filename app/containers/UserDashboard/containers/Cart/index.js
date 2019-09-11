25f8ca94b19547949436db0ede1281e8b3efcada
Sarik Dhungel <sareek007@gmail.com>
2019-09-11 16:45:28 +0545

    cart products api integrate

--- app/containers/UserDashboard/containers/Cart/index.js
+++ app/containers/UserDashboard/containers/Cart/index.js
@@ -13,9 +13,10 @@ import noreport from 'assets/images/report.png';
 import { Link } from 'react-router-dom'
 import './style.scss';
 import placeholder from './placeholder.png';
+import { DOCUMENT_URL_UPDATE } from '../../../App/constants';
 
 import {
-  
+  makeSelectCartProducts
 } from './selectors';
 // import { makeSelectLocation } from '../../../App/selectors';
 
@@ -23,101 +24,10 @@ import {
   getProductsInCartRequest
 } from './actions'
 
-const mockData = {
-  "status": 200,
-  "data": {
-    "dataList": [
-      {
-        "_id": "b7f9bbd0-d475-11e9-9703-878c003ca5c6",
-        "product": {
-          "_id": "691e8a20-ccca-11e9-be26-e979a8382fa2",
-          "added_by": "sareek007@gmail.com",
-          "added_on": "2019-09-01T15:08:53.058Z",
-          "deleted": false,
-          "country": "Nepal",
-          "industry": "Financial",
-          "title": "PCI DSS",
-          "description": "This products contains PCI related questionnaires.",
-          "price": 150,
-          "product_instructions": "",
-          "profile_name": "PCI_DSS_NP",
-          "category_id": [
-            "5926fff0-c750-11e9-b9fe-e1f61cc86d1e",
-            "a41791f0-c750-11e9-b9fe-e1f61cc86d1e",
-            "8308a970-c743-11e9-b9fe-e1f61cc86d1e",
-            "5febdca0-c743-11e9-b9fe-e1f61cc86d1e"
-          ],
-          "questions": [
-            "23ae56d0-ccd1-11e9-be26-e979a8382fa2",
-            "3c4d7090-ccd1-11e9-be26-e979a8382fa2",
-            "466d9a50-ccd1-11e9-be26-e979a8382fa2",
-            "5221d190-ccd1-11e9-be26-e979a8382fa2",
-            "5b8f9b40-ccd1-11e9-be26-e979a8382fa2",
-            "650d4370-ccd1-11e9-be26-e979a8382fa2",
-            "098ec980-cccf-11e9-be26-e979a8382fa2",
-            "0a5bc530-ccc9-11e9-be26-e979a8382fa2",
-            "13091650-cccf-11e9-be26-e979a8382fa2",
-            "02c9c6c0-ccd1-11e9-be26-e979a8382fa2",
-            "0dd2c9e0-ccd1-11e9-be26-e979a8382fa2",
-            "1a16e510-ccd1-11e9-be26-e979a8382fa2"
-          ],
-          "active": true,
-          "image_name": {
-            "document_name": "mcqExam-1567350532953-e11e0.png",
-            "document_original_name": "pcidss.png",
-            "document_mimetype": "image/png"
-          },
-          "questionnaire_id": "691e8a20-ccca-11e9-be26-e979a8382fa2",
-          "updated_by": "sareek007@gmail.com",
-          "updated_on": "2019-09-10T05:14:02.630Z"
-        }
-      },
-      {
-        "_id": "c324e260-d474-11e9-8293-01b9d353aff3",
-        "product": {
-          "_id": "d73fe490-ced7-11e9-a3dd-312005b9fdc3",
-          "added_by": "sareek007@gmail.com",
-          "added_on": "2019-09-04T05:50:03.609Z",
-          "deleted": false,
-          "country": "Algeria",
-          "industry": "Health Care and Biomedical",
-          "title": "Test Product",
-          "description": "test test test",
-          "price": 1200,
-          "product_instructions": "<p>this is test</p>",
-          "profile_name": "Salina",
-          "category_id": [
-            "a41791f0-c750-11e9-b9fe-e1f61cc86d1e"
-          ],
-          "questions": [
-            "098ec980-cccf-11e9-be26-e979a8382fa2",
-            "0a5bc530-ccc9-11e9-be26-e979a8382fa2",
-            "13091650-cccf-11e9-be26-e979a8382fa2",
-            "1872a490-ccc9-11e9-be26-e979a8382fa2",
-            "1b157b40-cccf-11e9-be26-e979a8382fa2",
-            "25eb8780-cccf-11e9-be26-e979a8382fa2",
-            "2cac6b90-cccd-11e9-be26-e979a8382fa2"
-          ],
-          "active": true,
-          "image_name": {
-            "document_name": "mcqExam-1567576201529-deeeb.jpeg",
-            "document_original_name": "card.jpeg",
-            "document_mimetype": "image/jpeg"
-          },
-          "questionnaire_id": "d73fe490-ced7-11e9-a3dd-312005b9fdc3",
-          "updated_by": "sareek007@gmail.com",
-          "updated_on": "2019-09-04T06:11:54.747Z"
-        }
-      }
-    ],
-    "totalItems": 2,
-    "currentPage": 1
-  }
-}
 
 const mapStateToProps = createStructuredSelector({
   // isRequesting: makeSelectLoading(),
-  
+  cartProducts:makeSelectCartProducts()
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -129,7 +39,7 @@ class Cart extends React.Component {
   constructor(props) {
     super(props);
       this.state = { 
-
+        data: {}
       };
   }
 
@@ -138,35 +48,42 @@ class Cart extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-
+    if (this.props.cartProducts != nextProps.cartProducts) {
+      this.setState({
+        data: nextProps.cartProducts && nextProps.cartProducts.toJS(),
+      });
+    }
   }
 
   render() {
     const {  } = this.props;
-    const { } = this.state
+    const { data } = this.state
     return (
           <div className="ui divided items">
-            {mockData.data.dataList.map((item, index) => {
+            {data && data.dataList && data.dataList.map((item, index) => {
               return (
                 <div className="item" key={index}>
               <div className="image">
-                <img src={placeholder}/>
+              <img src={`${DOCUMENT_URL_UPDATE}${item && 
+                                 item.product.image_name && 
+                                     item.product.image_name.document_name}`} 
+                                         alt="product image here" />
               </div>
               <div className="content">
-                <p className="header">Product Name</p>
+                <p className="header">{item.product.title}</p>
                 <div className="meta">
-                <span className="price">$1200</span>
+                <span className="price">${item.product.price}</span>
                 
                   {/* <span className="tag">Profile Name </span> */}
                 </div>
                 <div className="description">
-                  <p> Profile Name : $333  </p>
-                  <p>  This is a description</p> 
+                  <p> Profile Name :{item.product.profile_name} </p>
+                  <p>{item.product.description}</p> 
                   <br/>  
                 </div>
-                <button className="ui basic red labeled icon button delete-button">
+                <button onClick={this.removeCart} className="ui basic red labeled icon button delete-button">
                   <i className="delete icon"></i>
-                  Remove from Cart
+                    Remove from Cart
                 </button>
               </div>
             </div>
