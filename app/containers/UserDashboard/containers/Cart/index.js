4a48e914ffea1cbea49ca6475b26ae1aaeeb03a1
Sarik Dhungel <sareek007@gmail.com>
2019-09-12 11:42:34 +0545

    design integration

--- app/containers/UserDashboard/containers/Cart/index.js
+++ app/containers/UserDashboard/containers/Cart/index.js
@@ -77,37 +77,79 @@ class Cart extends React.Component {
     const {  } = this.props;
     const { data } = this.state
     return (
-          <div className="ui divided items">
+      <div class="cart-grid">
+      <div>
+      <div className="ui top attached header cart-heading">
+            <span> My Cart Items (2)</span>
+            <span className="right">Total: $666</span>
+          </div>
+
+          <div className="ui  divided items">
             {data && data.dataList && data.dataList.map((item, index) => {
               return (
                 <div className="item" key={index}>
-              <div className="image">
-              <img src={`${DOCUMENT_URL_UPDATE}${item && 
-                                 item.product.image_name && 
-                                     item.product.image_name.document_name}`} 
-                                         alt="product image here" />
-              </div>
-              <div className="content">
-                <p className="header">{item.product.title}</p>
-                <div className="meta">
-                <span className="price">${item.product.price}</span>
-                
-                  {/* <span className="tag">Profile Name </span> */}
+                  <div className="image">
+                  <img src={`${DOCUMENT_URL_UPDATE}${item && 
+                                    item.product.image_name && 
+                                        item.product.image_name.document_name}`} 
+                                            alt="product image here" />
+                  </div>
+                  <div className="content">
+                    <p className="header">{item.product.title}</p>
+                    <div className="meta">
+                    <span className="price">${item.product.price}</span>
+                    
+                      {/* <span className="tag">Profile Name </span> */}
+                    </div>
+                    <div className="description">
+                      <p> Profile Name :{item.product.profile_name} </p>
+                      <p>{item.product.description}</p> 
+                      <br/>  
+                    </div>
+                    <button onClick={() => this.removeCart(item._id) } className="ui basic red labeled icon button delete-button">
+                      <i className="trash alternate icon"></i>
+                        Remove from Cart
+                    </button>
+                  </div>
                 </div>
-                <div className="description">
-                  <p> Profile Name :{item.product.profile_name} </p>
-                  <p>{item.product.description}</p> 
-                  <br/>  
-                </div>
-                <button onClick={() => this.removeCart(item._id) } className="ui basic red labeled icon button delete-button">
-                  <i className="delete icon"></i>
-                    Remove from Cart
-                </button>
-              </div>
-            </div>
-    )
-  })}
+            )
+          })}
 
+</div>
+</div>
+<div className="order-detail">
+  <div className="ui card">
+    <div className="content grey-bg">
+      <div className="header">Price Details</div>
+    </div>
+    <div className="content">
+      <div className="pricing-grid">
+        <span className="">Order Total</span>
+        <span className="right">$666</span>
+      </div>
+      <div className="pricing-grid">
+        <span className="">Tax Payable</span>
+        <span className="right">$6</span>
+      </div>
+      <div className="pricing-grid">
+        <span className="">Discount</span>
+        <span className="right">N/A</span>
+      </div>
+      <div className="pricing-grid">
+        <span className="">Delivery Charge</span>
+        <span className="right">N/A</span>
+      </div>
+      <div className="pricing-grid total">
+        <span className="">Total</span>
+        <span className="right">$672</span>
+      </div>
+    </div>
+
+    <div className="extra content">
+      <button className="ui green labeled icon  button"><i className="cart arrow down icon"></i>Place Order</button>
+    </div>
+  </div>
+</div>
 </div>
     );
   }
