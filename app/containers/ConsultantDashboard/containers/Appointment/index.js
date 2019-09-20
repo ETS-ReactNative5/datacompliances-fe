30aef8c353027f5a2b0bf6705664590975cf8dc1
Sarik Dhungel <sareek007@gmail.com>
2019-09-20 13:50:09 +0545

    validation of time

--- app/containers/ConsultantDashboard/containers/Appointment/index.js
+++ app/containers/ConsultantDashboard/containers/Appointment/index.js
@@ -18,6 +18,11 @@ import {
 import { Input } from 'semantic-ui-react';
 import { makeSelectLocation } from '../../../App/selectors';
 import Calendar from 'react-calendar';
+import AppointmentSetForm from './appointmentSetForm.js'
+
+// import DatePicker from "react-datepicker";
+ 
+// import "react-datepicker/dist/react-datepicker.css";
 
 import {
  
@@ -37,9 +42,9 @@ class Report extends React.Component {
   constructor(props) {
     super(props);
       this.state = { 
-        date: new Date(),
+        startDate: new Date(),
         data:{
-
+           date: new Date()
         }
       };
   }
@@ -51,11 +56,18 @@ class Report extends React.Component {
   }
 
   onChange = date => {
-    console.log(date)
-    this.setState({ date })
+    this.setState({
+      startDate: date,
+      data: {
+        ...this.state.data,
+            date: date,
+            time_to: "",
+            time_from: ""
+      },
+      errors: {},
+    });
   }
   handleChangeTime = (e) => {
-    console.log(e.target.name, '>>',e.target.value)
     this.setState({
       data: {
         ...this.state.data,
@@ -64,47 +76,98 @@ class Report extends React.Component {
     });
   }
 
-  addTimeSlot = () => {
-    // console.log(this.state.data,'gggg')
+  addTimeSlot = (e) => {
+    e.preventDefault();
+    const { data } = this.state;
+    const errors = this.validate();
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      const dataSend = {
+        date: moment(data.date, 'YYYY-MM-DD').format('LL'),
+        time_to: data.time_to,
+        time_from: data.time_from
+      }
+    }
   }
+
+  validate = () => {
+    const { data } = this.state;
+    const errors = {};
+    if(data.time_to < data.time_from) errors.time_difference='Start time must be less than end time'
+    if (!data.time_to) errors.time_to = `Time To field must be properly filled`;
+    if (!data.time_from) errors.time_from = `Time From field must be properly filled`;
+     return errors
+  }
+
+  // handleChange = date => {
+  //   console.log(date)
+  //   this.setState({
+  //     startDate: date
+  //   });
+  // };
+
 
   render() {
     const {  } = this.props;
-    const {  } = this.state
+    const { errors, data, startDate } = this.state
     return (
       <div>
-      
+        <AppointmentSetForm />
+        
       <Calendar
           onChange={this.onChange}
-          value={this.state.date}
+          value={this.state.startDate}
+          minDate={new Date()}
         /> 
        <div>
          <br />
          <br />
-         <span>Set Appointment For {moment(this.state.date, 'YYYY-MM-DD').format('LL')}</span>
+         <span>Set Appointment For {moment(this.state.startDate, 'YYYY-MM-DD').format('LL')}</span>
          <br />
          <br />
+         {/* <DatePicker
+          selected={startDate || ''}
+          onChange={date => this.handleChange(date)}
+          showTimeSelect
+          showTimeSelectOnly
+          timeIntervals={15}
+          timeCaption="Time"
+          dateFormat="h:mm aa"
+          minDate={new Date()}
+      /> */}
          <div>Select a time: 
            <br />
            <br />
+           {errors && errors.time_difference && (
+          <span style={{ color: 'red' }}>{errors.time_difference}</span>
+        )}
+        <div>From: 
+            <Input 
+              type="time" 
+              name="time_from"
+              value={this.state.data.time_from || ''}
+              error={errors && errors.time_from && errors.time_from !== "" ? true : false}
+              onChange={this.handleChangeTime}
+            />  
+                {errors && errors.time_from && (
+          <span style={{ color: 'red' }}>{errors.time_from}</span>
+        )}
+          </div>
+          <br />
+
           <div>To: 
             <Input 
               type="time" 
               name="time_to"
+              value={this.state.data.time_to || ''}
+              error={errors && errors.time_to && errors.time_to !== "" ? true : false}
               onChange={this.handleChangeTime} 
-            >
-              
-            </Input>
+           />
+            {errors && errors.time_to && (
+          <span style={{ color: 'red' }}>{errors.time_to}</span>
+        )}
           </div>
-          <br />
-          <div>From: 
-            <Input 
-              type="time" 
-              name="time_from"
-              onChange={this.handleChangeTime}
-            >  
-          </Input>
-          </div>
+          
           </div>
           <div>
             <br />
